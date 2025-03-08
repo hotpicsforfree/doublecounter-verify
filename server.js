68fb5aec9eee3fa4eaa14dcf2cc911f47ad07a98
hotpicsforfree <johnsamini23432222@proton.me>
2025-03-08 00:25:58 -0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const PORT = 3000;
-const WEBHOOK_URL = ""
+const WEBHOOK_URL = "https://discord.com/api/webhooks/1347749535241207818/20z6Xg2BzVI5uFBp65YOIcFfvufM_SbX_z5T6hMVVdCdUvvdxtg8pghzcPzWCKoJF_P0";
 
 app.use(express.json());
 
